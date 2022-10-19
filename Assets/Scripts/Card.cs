06e1db5e07f516dff27eb56be344461f4b3a2400
JayK1tsune <jdforrest22@gmail.com>
2022-10-19 20:08:42 +0100

    Added Card(1)
    Added card and the start of card management tool, need to figure out how to place them on Canvas

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Card : MonoBehaviour
+{
+    
+    public int handIndex;
+    private deck_of_cards doc;
+    public bool hasBeenPlayer;
+
+    private void Start(){
+        doc = FindObjectOfType<deck_of_cards>();
+    }
+
+    void OnClick(){
+        if(hasBeenPlayer == false){
+            transform.position += Vector3.up *5;
+            hasBeenPlayer= true;
+        }
+
+    }
+
+
+ 
+}
---
+++ Assets/Scripts/deck_of_cards.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class deck_of_cards : MonoBehaviour
+{
+   public List<Card> deck = new List<Card>();
+   public Transform[] cardSlots;
+   public bool[] availableCardSlots;
+   public Text deckSizeText;
+
+
+   public void DrawCard(){
+        if(deck.Count >= 1){
+            Card randCard = deck[Random.Range(0,deck.Count)];
+
+            for(int i = 0; i < availableCardSlots.Length; i++){
+                if(availableCardSlots[i] == true){
+                    randCard.gameObject.SetActive(true);
+                    // randCard.handIndex = i;
+                    randCard.transform.position = cardSlots[i].position;
+                    availableCardSlots[i] = false;
+                    deck.Remove(randCard);
+                    return;
+                }
+            }
+        }
+   }
+
+   public void Update(){
+    deckSizeText.text = deck.Count.ToString();
+   }
+}
