12548a3e4f02d2df82a9408c395fba49c970e447
Jay <91475458+JayK1tsune@users.noreply.github.com>
2022-11-24 14:33:56 +0000

    Update Exit_Spawn.cs

--- Assets/Scripts/Exit_Spawn.cs
+++ Assets/Scripts/Exit_Spawn.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 public class Exit_Spawn : MonoBehaviour
 {
     [SerializeField] private GameObject Spawn;
+    [SerializeField] private GameObject player;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        Spawn.GetComponent<CircleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(player){
+            Debug.Log("Entered");
+        }
+    }
+
+    
 }
