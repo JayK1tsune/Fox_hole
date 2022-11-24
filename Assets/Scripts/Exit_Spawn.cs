5422478ce91b9e9910871d8a3577393281cce051
Jay <91475458+JayK1tsune@users.noreply.github.com>
2022-11-24 13:35:06 +0000

    2022.1.7f1
    2022.1.7f1

---
+++ Assets/Scripts/Exit_Spawn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit_Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject Spawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
